1aebb59de05681df2c0207b7c77cf56663711ac8
MamutA14 <alam@ciencias.unam.mx>
2021-12-08 18:18:39 -0600

    Se acaba de crear la opcion update

---
+++ client/client.py
@@ -0,0 +1,82 @@
+import socket
+import os 
+import sys
+import time 
+import struct
+import tqdm
+import errno
+
+class ClientFTP(object):
+    
+    def __init__(self, ip,port,buffer=1024):
+        #el guión bajo sirve para hacer la variables privadas
+        self.__ip=ip
+        self.__port=port
+        self.__buffer=buffer
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    #Se establecera la conexión aquí
+    def connect(self):
+        try:
+            self.socket.connect((self.__ip,self.__port))
+            print("[SUCCESSFUL CONECTION]")
+            client.socket.send("[SUCCESSFUL CONECTION]".encode())
+        except Exception as e:
+            print(str(e))
+            print("[ERROR CONECTION]")
+            client.socket.send("[ERROR CONECTION]".encode())    
+    
+    
+    def update(self,filename):
+            clientFile = "../dataC/"+ filename
+            file = open(clientFile,"r")
+            data = file.read()
+            message = "UP"+","+filename
+            try:
+                client.socket.send(message.encode("utf-8"))
+                msg = client.socket.recv(1024).decode("utf-8")
+                print(f"[SERVER] {msg}")
+                
+                client.socket.send(data.encode("utf-8"))
+                msg = client.socket.recv(1024).decode("utf-8")
+                print(f"[SERVER]:{msg}")
+            except IOError as e:
+                 if e.errno == errno.EPIPE:
+                    print("De nuevo pipe")
+            
+             
+            """
+            try:
+                message = "UP"+","+filename
+                self.socket.send(message.encode())
+            except Exception as e:
+                print(str(e))
+            self.socket.send(struct.pack("i",len(filename.encode())))
+            self.socket.send(filename.encode())
+            filesize = os.path.getsize(filename)
+            progress = tqdm.tqdm(range(filesize),f"Enciends {filename}",unit_scale=True,unit_divisor=1024)
+            with open(filename,'rb') as file:
+                self.socket.send(struct.pack("i",filesize))
+                for x in progress:
+                    bytes_read = file.read(self.__buffer)
+                    if not bytes_read:
+                        break
+                    self.socket.sendall(bytes_read)
+                    progress.update(len(bytes_read))   
+                    """ 
+                
+if __name__ == '__main__':
+    IP = socket.gethostbyname(socket.gethostname()) #Obtiene la dirección ip a traves del nombre del host (esto ya que el valor de IP puede variar dependiendo de SO)
+    PORT = 2350
+    client = ClientFTP(IP,PORT)
+    print( "------[Client ftp]")
+    while True:
+        command = input ("[Insert command]: ")
+        if command.upper() == "CONN":
+            print("Sending Query Connection")
+            client.connect()
+        elif command.upper() == "UP":
+            filename = input("Insert name file: ")
+            client.update(filename)
+            
+        else:
+            client.socket.send(command.encode())
---
+++ server/server.py
@@ -0,0 +1,95 @@
+from typing import TYPE_CHECKING 
+if TYPE_CHECKING: 
+    from _typeshed import Self
+import socket
+import os 
+import sys
+import time 
+import struct
+from  dataclasses import dataclass
+
+@dataclass
+class ServerFTP(object):
+    
+    def __init__(self, ip,port,buffer=1024):
+        #el guión bajo sirve para hacer la variables privadas
+        self.__ip=ip
+        self.__port=port
+        self.__buffer=buffer
+        self.socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEPORT,1)
+        self.conn=None
+        self.addr=None
+        self.data_address=None
+        self.datasock=None
+        self.mode='I'
+    
+    def freeze(self):
+        self.socket.bind((self.__ip,self.__port) ) #Mantiene la conexión abierta en esa dirección
+        self.socket.listen(1)#escucha la conexión
+        self.conn, self.data_address = self.socket.accept() #Se ha abierto una nueva conexión lo cual implica una nueva dirección(ip, puerto) y esta función regresa un socket que permite el envio de datos a está nueva dirección  
+    #Se va a encargar de los mensajes que le lleguen al socket 
+    def receive(self):
+        return self.conn.recv(self.__buffer )
+    
+    def upload(self,data):
+        filename = data[1]
+        #
+        dirS = "../dataS/"
+        #Se crea un nuevo directorio para almacenar los archivos subidos
+        try:
+            if(not (os.path.exists(dirS)) ):
+                os.mkdir(dirS) 
+            serveFile = dirS+filename
+            print("[RECV] Filename received.")
+            file = open(serveFile,"w") #Se creara un nuevo documento con ese nombre y dirección
+            self.conn.send("Filename received".encode("utf-8"))
+            body = self.receive().decode("utf-8")
+            print(f"[RECV] File data received.")
+            file.write(body)
+            self.conn.send("File data received".encode("utf-8"))
+            file.close()
+        except OSError as e:
+            print(str(e))
+            print("Hubo un problema al crear el direcrtorio dataS")
+        
+             
+        """print(f"[LOADING FILE]: {filename}")
+        self.conn.send("OPEN CONNECTION".encode())
+        self.datasock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        readmode = "wb" if self.mode == "I" else  "w"
+        try:
+            with open(filename,readmode) as file:
+                while True:
+                    byte_recived = self.datasock.recv(self._buffer)
+                    if not byte_recived: break
+                    file.write(byte_recived)
+            self.conn.send("[Sucessful Transfer]")    
+        except Exception as e:
+            self.conn.send("[Error access file]")
+            print(str(e))
+        finally:
+            self.datasock.close()
+            print("Succesful onload")   """  
+            
+        
+if __name__ == '__main__':
+    IP = socket.gethostbyname(socket.gethostname()) #Obtiene la dirección ip a traves del nombre del host (esto ya que el valor de IP puede variar dependiendo de SO)
+    PORT = 2350
+    print(f"[LISTENING] Server is listening on {IP} : {PORT}")
+    server = ServerFTP(IP,PORT)
+    server.freeze() 
+    print("[Waiting query...]")
+    #Se genera un paso de escucha
+    while True:
+        data = server.receive()
+        print('Query: {0}'.format(data)) 
+        data = data.decode('utf-8').split(',')
+        if "UP"in data:
+            server.upload(data)
+        
+        elif "EXIT" in data:
+            pass #Desconectar    
+            
+        
+               
